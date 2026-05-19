86327ea6c46a69aa1088b6dc6824bf32faba9812
apple <apple@Vaibz-Mac.local>
2026-05-19 13:19:27 +0530

    new backend details updated

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-s3-backend-3-tier-app"
+    bucket = "terraform-s3-backend-3-tier-app-391623252926-ap-south-1-an"
     key    = "backend-locking"
     region = "ap-south-1"
     use_lockfile = true
